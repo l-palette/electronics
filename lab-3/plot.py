88f7a370c106e6b453f3a2223ce21cb2da3a7e5f
l-palette <lv-ttn@yandex.ru>
2026-06-25 23:18:46 +0300

    lab-3

--- lab-3/plot.py
+++ lab-3/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def build_plot(x, y, title, xlabel, ylabel, png, color='blue'):
     plt.figure(figsize=(8, 5))
-    plt.plot(x, y, 'o', color=color, markersize=8, label='Измерения')
+    plt.plot(x, y, '-o', color=color, markersize=8, label='Измерения')
     plt.xlabel(xlabel, fontsize=12)
     plt.ylabel(ylabel, fontsize=12)
     plt.title(title, fontsize=14)
@@ -14,28 +14,28 @@ def build_plot(x, y, title, xlabel, ylabel, png, color='blue'):
     plt.show()
 
 
-Ib_no_load = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-Ic_no_load = [12, 23, 40, 54, 70, 84]
+Ib_no_load = [0, 0.06, 0.28, 0.4, 0.5, 0.6]
+Ic_no_load = [0, 14, 35, 50, 76, 99]
 
 build_plot(
     Ib_no_load,
     Ic_no_load,
-    "Характеристика прямой передачи без нагрузки",
-    "Ток базы, $I_B$ (мА)",
-    "Ток коллектора, $I_C$ (мА)",
+    "Характеристика прямой передачи",
+    "Ток базы, $I_б$ (мА)",
+    "Ток коллектора, $I_к$ (мА)",
     "_assets/bjt_transfer_no_load.png",
     color='blue'
 )
 
-Ib_load = [0, 0.02, 0.04, 0.08, 0.10, 0.14, 0.18, 0.20, 0.40, 0.60]
-Ic_load = [0, 2, 4, 8, 10, 16, 20, 20, 20, 20]
+Ib_load = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+Ic_load = [0, 12,  22,  36,  41,  42,  42,  42,  42,  42]
 
 build_plot(
     Ib_load,
     Ic_load,
-    "Характеристика прямой передачи при наличии нагрузки",
-    "Ток базы, $I_B$ (мА)",
-    "Ток коллектора, $I_C$ (мА)",
+    "Характеристика прямой передачи с нагрузкой ($R_к = 150$ Ом)",
+    "Ток базы, $I_б$ (мА)",
+    "Ток коллектора, $I_к$ (мА)",
     "_assets/bjt_transfer_load.png",
     color='red'
 )
